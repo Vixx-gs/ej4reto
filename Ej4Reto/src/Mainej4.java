5d20b4170dbd61fac56c6f861c3d89a1512cc39d
Vixx-gs <victorgallegoserrate@gmail.com>
2024-11-19 11:55:08 +0100

    main mal hecho

--- Ej4Reto/src/Mainej4.java
+++ Ej4Reto/src/Mainej4.java
@@ -8,15 +8,7 @@ public class Mainej4 {
 				Scanner sc = new Scanner(System.in);
 				int n = funciones.dimeEntero("Introduce un numero: 1- Es Primo 2- Mayor 3- Menor", sc);
 				if(n==1) {
-					
-				}
-				
-				if(n==2) {
-					
-				}
-				
-				if(n==3) {
-					
+					System.out.println(FuncionesEj4.esPrimo("¿es primo?" , n));
 				}
 				
 				if(n==0) {
