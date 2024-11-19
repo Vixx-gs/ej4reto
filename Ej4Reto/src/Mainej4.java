e5edc86f48a7998fa0a724001b36573a02c73b28
Vixx-gs <victorgallegoserrate@gmail.com>
2024-11-19 11:41:25 +0100

    main con errores sin acabar

--- Ej4Reto/src/Mainej4.java
+++ Ej4Reto/src/Mainej4.java
@@ -1,9 +1,32 @@
+import java.util.Scanner;
 
 public class Mainej4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		do {
+			try {
+				Scanner sc = new Scanner(System.in);
+				int n = funciones.dimeEntero("Introduce un numero: 1- Es Primo 2- Mayor 3- Menor", sc);
+				if(n==1) {
+					
+				}
+				
+				if(n==2) {
+					
+				}
+				
+				if(n==3) {
+					
+				}
+				
+				if(n==0) {
+					break;
+				}
+			}catch Exception;
+			
+		}while(true);
+		
+		
 	}
 
 }
